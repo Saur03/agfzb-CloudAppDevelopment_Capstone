bd05effd320a401a9cabb563df6c81e67d22aaca
Saur03 <117138035+Saur03@users.noreply.github.com>
2023-12-31 16:08:55 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,33 +2,145 @@ import requests
 import json
 # import related models here
 from requests.auth import HTTPBasicAuth
-
+from .models import CarDealer, DealerReview
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-
+def get_request(url, api_key=None, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # If an API key is provided, use authentication
+        if api_key:
+            response = requests.get(
+                url,
+                headers={'Content-Type': 'application/json'},
+                params=kwargs,
+                auth=HTTPBasicAuth('apikey', api_key)
+            )
+        else:
+            # If no API key is provided, make a regular GET request
+            response = requests.get(
+                url,
+                headers={'Content-Type': 'application/json'},
+                params=kwargs
+            )
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, json_payload, **kwargs):
+    print(kwargs)
+    print("POST to {} ".format(url))
+    try:
+        # Call post method of requests library with URL, JSON payload, and parameters
+        response = requests.post(url, json=json_payload, headers={'Content-Type': 'application/json'},
+                                 params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
-
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, **kwargs)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["rows"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+    return results
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
+# restapis.py
+
+def get_dealer_reviews_from_cf(url, dealer_id, api_key):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, dealerId=dealer_id)
+    if json_result:
+        # Get the row list in JSON as reviews
+        reviews = json_result["reviews"]
+        # For each review object
+        for review in reviews:
+            # Create a DealerReview object with values in the review object
+            review_obj = DealerReview(
+                dealership=review["dealership"],
+                name=review["name"],
+                purchase=review["purchase"],
+                review=review["review"],
+                purchase_date=review["purchase_date"],
+                car_make=review["car_make"],
+                car_model=review["car_model"],
+                car_year=review["car_year"],
+                sentiment=None  # Initialize sentiment as None
+            )
+            
+            # Analyze sentiment and assign to the review object
+            review_obj.sentiment = analyze_review_sentiments(api_key, review_obj.review)
+
+            results.append(review_obj)
+    return results
+
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(api_key, text):
+    # Watson NLU URL for analyzing sentiment
+    url = "your_watson_nlu_url/v1/analyze"
+
+    # Parameters for the Watson NLU request
+    params = {
+        "text": text,
+        "version": "2021-03-25",  # Specify the version of Watson NLU
+        "features": "emotion,sentiment",
+        "return_analyzed_text": True
+    }
+
+    try:
+        # Call get_request with Watson NLU URL and parameters
+        response = get_request(url, api_key=api_key, **params)
+
+        # Check if the response contains sentiment information
+        if "sentiment" in response:
+            return response["sentiment"]
+        else:
+            return None
+    except Exception as e:
+        print(f"Error analyzing sentiment: {e}")
+        return None
+
+
+
 
 
 
